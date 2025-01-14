af5c1714d14b5f5bb8b15979f79a1a2b8dca9203
Chani-R <p0527672728@gmail.com>
2025-01-15 01:36:07 +0200

    update service

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5159'; // עדכן את ה-port לפי הצורך
-axios.defaults.baseURL = API_BASE_URL;
+// const API_BASE_URL = 'http://localhost:5159'; // עדכן את ה-port לפי הצורך
+// axios.defaults.baseURL = API_BASE_URL;
 
-const apiUrl = "http://localhost:5159"
+// const apiUrl = "http://localhost:5159"
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+axios.defaults.baseURL = "https://"+apiUrl;  // הגדרת baseURL לפי משתנה הסביבה או URL ברירת המחדל
+console.log("API Base URL:", apiUrl);
+axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 axios.interceptors.response.use(
   response => response,
